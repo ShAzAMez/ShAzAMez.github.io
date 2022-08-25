ec1578920ebb2a71ce004b35f3b3db14ad0f2b9e
ShAzAMez <93126110+ShAzAMez@users.noreply.github.com>
2022-08-25 16:25:11 +0300

    Add files via upload

---
+++ appik.js
@@ -0,0 +1,22 @@
+let bulls = 0
+const hung = 100
+const zero = 0
+let a = confirm("есть ли у тибя паринь???");
+if (a == true) {
+document.write ("твои баллы за 1 вопрос: " + hung + ". ")
+}
+else
+document.write ("твои баллы за 1 вопрос: " + zero + ". ")
+let b = confirm("циловалясь ли ти когда нибуть???")
+if (b == true) {
+document.write ("твои баллы за 2 вопрос: " + hung + ". ")
+}
+else 
+document.write ("твои баллы за 2 вопрос: " + zero + ". ")
+let c = confirm("хотела бы зостка джага джага мишку???")
+if (c == true) {
+document.write ("твои баллы за 3 вопрос: " + hung + ". ")
+}
+else 
+document.write ("твои баллы за 3 вопрос: " + zero + ". ")
+document.write ("общие твои баллы: ОТИНЬ МАЛА БАЛЛЛАВ ")
